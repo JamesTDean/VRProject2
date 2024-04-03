61353672426a550ef8c8e6e49b9ce60d7a76a2be
JamesTDean <jtdean52@gmail.com>
2024-04-02 19:10:15 -0500

    Added Conveyer Belt

---
+++ Assets/Scripts/ConveyerScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerScript : MonoBehaviour
+{
+    public Vector3 forceDirection;
+    private float forceMagnitude;
+
+    void Start()
+    {
+        forceMagnitude = 5f;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.gameObject.ToString());
+
+        if (other.transform.tag == "Ball")
+        {
+            Rigidbody ballRB = null;
+            try
+            {
+                ballRB = other.gameObject.GetComponent<Rigidbody>();
+            }
+            catch
+            {
+                print("No rigid body component.");
+            }
+            if (ballRB != null)
+            {
+                ballRB.AddForce(forceMagnitude * forceDirection);
+            }
+        }
+    }
+
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.ToString());
+
+        if (other.transform.tag == "Ball")
+        {
+            Rigidbody ballRB = null;
+            try
+            {
+                ballRB = other.gameObject.GetComponent<Rigidbody>();
+            }
+            catch
+            {
+                print("No rigid body component.");
+            }
+            if (ballRB != null)
+            {
+                ballRB.AddForce(forceMagnitude * forceDirection);
+            }
+        }
+    }
+    */
+}
